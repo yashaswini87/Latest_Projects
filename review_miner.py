20b78552f56d1f1fa5998d4ff2e194fd33ef499f
ykotres <ykotresh@walmartlabs.com>
2015-06-26 14:42:27 -0700

    Adding current changes

--- review_miner.py
+++ review_miner.py
@@ -1,10 +1,30 @@
+import nltk
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer
-
+nltk.download('http://nltk.org/data.html')
 pos = 0
 neg = 0
 nouns_and_noun_phrases = []
-with open("reviews/reviews_ipad.txt") as review_file:
+
+
+import urllib2
+import json
+quoted_key='1EOLVBF5QVVT'
+url='http://catalog.prod.cdqarth.prod.walmart.com/catalog-service/v0.3/catalog/products/search?filter=product_id=%s&tenantId=0' %quoted_key
+response=urllib2.urlopen(url, timeout=10)
+spell_json=json.load(response)
+features= spell_json['docs'][0]['product_attributes'].keys()
+# features=["size","wetness indicator","absorb","quality","deliver","ship","offer","softness","weight","price"]
+    
+feature_sent_dict={}
+for feature in features:
+    feature_list=feature.split('_')
+    for f in feature_list:
+        feature_sent_dict[f]={}
+        feature_sent_dict[f]['pos']=0
+        feature_sent_dict[f]['neg']=0
+    
+with open("./reviews/reviews_diapers_28240450.txt") as review_file:
     for line in review_file:
         review = line.strip()
         review_blob = TextBlob(review)
@@ -18,21 +38,20 @@ with open("reviews/reviews_ipad.txt") as review_file:
             for token, tag in sentence.tags:
                 if 'NN' in tag:
                     sentence_features["nouns"].append(token)
-            if "screen" in sentence or "display" in sentence:
-                pol = sentence.sentiment.polarity
-
-                if pol > 0:
-                    pos += 1
+            for feature in features:
+                if feature in sentence :
+                    pol = sentence.sentiment.polarity 
+                    if pol > 0:
+                        feature_sent_dict[feature]['pos']+=1
+                    
                     # print sentence
-                if pol < 0:
-                    neg += 1
+                    if pol < 0:
+                        feature_sent_dict[feature]['neg']+=1
                     print sentence
-
-
             nouns_and_noun_phrases.append(' '.join(sentence_features["noun_phrases"] + sentence_features["nouns"]))
-
-print "Positive", pos
-print "Negative", neg
-
+# 
+# print "Positive", pos
+# print "Negative", neg
+print feature_sent_dict
 cv = CountVectorizer(ngram_range=(1,2), min_df=0.01)
 cv.fit_transform(nouns_and_noun_phrases)
--- scripts/get_reviews.py
+++ scripts/get_reviews.py
@@ -1,13 +1,13 @@
 import requests
 
-product_id = "4408441"
+product_id = "28240450"
 
 url = "http://api.meta-data.glb.prod.walmart.com/reviews/" + product_id + "?page="
 
 num_pages = int(requests.get(url + "1").json().get("payload").get("pagination").get("pages")[-1].get("num"))
 review_corpus = []
 
-with open('../reviews/reviews_bed_4408441.txt', 'w') as review_file:
+with open('../reviews/reviews_diapers_28240450.txt', 'w') as review_file:
     for page in range(1, num_pages):
         page_url = url + str(page)
         reviews = requests.get(page_url).json().get("payload").get("customerReviews")

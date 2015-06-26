a941629467877edd4b2136f8c78e7d6d668e9ab8
amore <amore@walmartlabs.com>
2015-06-26 12:04:45 -0700

    feature based review

--- review_miner.py
+++ review_miner.py
@@ -1,7 +1,8 @@
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer
 
-
+pos = 0
+neg = 0
 nouns_and_noun_phrases = []
 with open("reviews/reviews_ipad.txt") as review_file:
     for line in review_file:
@@ -17,5 +18,21 @@ with open("reviews/reviews_ipad.txt") as review_file:
             for token, tag in sentence.tags:
                 if 'NN' in tag:
                     sentence_features["nouns"].append(token)
-            print sentence_features
+            if "screen" in sentence or "display" in sentence:
+                pol = sentence.sentiment.polarity
 
+                if pol > 0:
+                    pos += 1
+                    # print sentence
+                if pol < 0:
+                    neg += 1
+                    print sentence
+
+
+            nouns_and_noun_phrases.append(' '.join(sentence_features["noun_phrases"] + sentence_features["nouns"]))
+
+print "Positive", pos
+print "Negative", neg
+
+cv = CountVectorizer(ngram_range=(1,2), min_df=0.01)
+cv.fit_transform(nouns_and_noun_phrases)

21f6b7647c8b42e1a18dc36a88bcd5a888235930
ykotres <ykotresh@walmartlabs.com>
2015-06-27 18:27:11 -0700

    review_miner with wordcloud

--- review_miner.py
+++ review_miner.py
@@ -4,6 +4,9 @@ import urllib2
 import pandas as pd
 import json
 from nltk.corpus import stopwords
+import sentence_clustering 
+from random import randint
+import wordcloud
 # from scripts import get_reviews
 
 selected_features = {}
@@ -35,11 +38,12 @@ def get_features(wpid):
     response=urllib2.urlopen(url, timeout=10)
     spell_json=json.load(response)
     features= spell_json['docs'][0]['product_attributes'].keys()
+    product_title=str(spell_json['docs'][0]['product_attributes']['product_name']['values'][0]['value'])
 # features=["size","wetness indicator","absorb","quality","deliver","ship","offer","softness","weight","price"]
     feature_list=["ship", "price","size","quality","deliver","store","warrant","assembl","remote control","ship to store","store pickup","pickup in store","pick up in store"]
     for feature in features:
         feature_list.extend(feature.split('_'))
-    return list(set(feature_list))
+    return list(set(feature_list)),product_title
 
 def modify_features(feature_list):
     new_feature_list=[]
@@ -55,23 +59,26 @@ def modify_features(feature_list):
 
 def feature_senti(reviewfile,feature_list,outputfile=None):
     feature_sent_dict={}
+    feature_wordcloud_dict={}
     for f in feature_list:
         f=f.lower()
         feature_sent_dict[f]={}
         feature_sent_dict[f]['pos']=0
         feature_sent_dict[f]['neg']=0
+        feature_wordcloud_dict[f]=[]
     with open(reviewfile,'r') as review_file:
         for line in review_file:
-            review = line.strip()
+            review = line.rstrip()
             review_blob = TextBlob(review)
             for sentence in review_blob.sentences:
                 try:
                     for feature in feature_sent_dict.keys():
                         if feature in sentence :
                             pol = sentence.sentiment.polarity 
+                            if (pol>= 0.70) and (pol <= 0.70) and len(sentence) < 80:
+                                feature_wordcloud_dict[feature].append(str(sentence))
                             if pol > 0:
                                 feature_sent_dict[feature]['pos']+=1
-                                
                             # print sentence
                             if pol < 0:
                                 feature_sent_dict[feature]['neg']+=1
@@ -82,7 +89,7 @@ def feature_senti(reviewfile,feature_list,outputfile=None):
     df.cols=['feature','neg','pos']
     df.sort(['pos','neg'],ascending=[False,False],inplace=True)
     df.to_csv(outputfile)
-    return feature_sent_dict
+    return feature_sent_dict,feature_wordcloud_dict
 
 def get_feature_senti(productid, get_reviews=False):
     review_file = './reviews/reviews_{}.txt'.format(productid)
@@ -91,18 +98,21 @@ def get_feature_senti(productid, get_reviews=False):
         get_reviews.get_reviews(productid,review_file)
     wpid=get_wmid(productid)
     wpid=str(wpid.pop())
-    feature_list=get_features(wpid)
+    feature_list,product_title=get_features(wpid)
     feature_list=modify_features(feature_list)
     outputfile='./reviews/sentiment_%s.txt' %productid
-    return feature_senti(review_file,feature_list,outputfile=outputfile)
+    feature_info,feature_wordcloud_dict=feature_senti(review_file,feature_list,outputfile=outputfile)
+    return feature_info,feature_wordcloud_dict,product_title
 
 if __name__=='__main__':
-    product_id = '29010048'
-    feature_info = get_feature_senti(product_id)
+    product_id = '4408441'
+    feature_info,feature_wordcloud_dict,product_title = get_feature_senti(product_id)
     review_summary = {"product_id": product_id}
+    review_summary={"product_title":product_title}
     data = []
     series = [{"name": "Positive", "data": []}, {"name": "Negative", "data": []}]
     categories = []
+    feature_wordcloud_dict={k:v for k,v in feature_wordcloud_dict.iteritems() if k in selected_features[product_id]}
     for feat_info in feature_info.iterkeys():
         name = feat_info
         d = feature_info[feat_info]
@@ -110,17 +120,27 @@ if __name__=='__main__':
         neg = d["neg"]
         if feat_info in selected_features[product_id]:
             name=name if name not in feature_name_mapping else feature_name_mapping[name]
-            data_point = {}
-            data_point["name"] = name 
-            data_point['y'] = pos + neg
-            data.append(data_point)
+#             data_point = {}
+#             data_point["name"] = name 
+#             data_point['y'] = pos + neg
+#             data.append(data_point)
             categories.append(name.title())
             series[0]["data"].append(pos)
             series[1]["data"].append(neg)
-    review_summary["data"] = data
+#     review_summary["data"] = data
     review_summary["col_data"] = series
     review_summary["col_cats"] = categories
 
-with open("new_review_data.txt", 'a') as review_data_file:
-    review_data_file.write(json.dumps(review_summary) + '\n')
+    with open("review_data.txt", 'a') as review_data_file:
+        review_data_file.write(json.dumps(review_summary) + '\n')
+
+    print '########### Word cloud ###################################'
+    tags=sentence_clustering.main(feature_wordcloud_dict,5)
+    tags_new=[]
+    for sentence in tags:
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+        size = randint(50,200)
+        tags_new.append({'color': (r, g, b), 'tag': sentence, 'size': size})
+    cloud_output='./ui/cloud_%s.html'%product_id
+    wordcloud.generate_tag_cloud(tags_new,cloud_output)
 #     get_feature_senti('28240450','./reviews/reviews_diapers_28240450.txt')
